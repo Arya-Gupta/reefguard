a599d4241157f353a544d0f208b961bd07df3933
Arya Gupta <arya.gupta99999@gmail.com>
2025-11-25 21:10:39 -0500

    feat: add HomeView to render main home page with featured articles and recent events

--- core/views.py
+++ core/views.py
@@ -1,1 +1,16 @@
-# Placeholder
+from django.views.generic import TemplateView
+from .models import Reef, Event, Article
+
+class HomeView(TemplateView):
+    """Home page view with featured content and recent activity."""
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['featured_articles'] = Article.objects.filter(
+            published=True, featured=True
+        )[:3]
+        context['recent_events'] = Event.objects.select_related('reef')[:5]
+        context['reef_count'] = Reef.objects.count()
+        context['event_count'] = Event.objects.count()
+        return context
